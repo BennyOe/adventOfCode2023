811b34de1871402164eae7355668b9d46fc63d51
Benjamin <b.oechsle@web.de>
2024-04-29 14:02:32 +0200

    Day 2 start

---
+++ AdventOfCode/src/main/kotlin/Day2.kt
@@ -0,0 +1,7 @@
+object Day2 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // Bag has been loaded with only 12 red cubes, 13 green cubes, 14 blue cubes
+        // TODO what is the sum of the IDs of the games that would have been possible
+    }
+}
