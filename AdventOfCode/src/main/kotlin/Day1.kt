e70bd2976608b3e6991e99a3d9df6e322e3dab3a
Benjamin <b.oechsle@web.de>
2024-04-29 13:41:47 +0200

    Day 1 complete

--- AdventOfCode/src/main/kotlin/Day1.kt
+++ AdventOfCode/src/main/kotlin/Day1.kt
@@ -3,16 +3,29 @@ import java.io.File
 object Day1 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val textList: List<String> = File("src/main/resources/day1/input").useLines { it.toList() }
-        var res = 0
-        println("Hallo Jan")
-
-        val digitList = textList.forEach { line ->
-            val result = line.filter { it.isDigit() }
-                val pair = "${result.first()}${result.last()}"
-                res += pair.reduce { acc, c -> acc + c.code }
-                .code
-        }
-        println(res)
+        // Part one
+        println(File("src/main/resources/day1/input").useLines { lines ->
+            lines.sumOf { line ->
+                line.filter { it.isDigit() }.let { "${it.first()}${it.last()}" }.toInt()
+            }
+        })
+        // Part two
+        println(File("src/main/resources/day1/input").useLines { lines ->
+            lines.sumOf { line ->
+                line.replaceSpelledDigits().filter { it.isDigit() }.let { "${it.first()}${it.last()}" }.toInt()
+            }
+        })
     }
+}
+
+private fun String.replaceSpelledDigits(): String {
+    return replace("nine", "n9e")
+        .replace("eight", "e8t")
+        .replace("seven", "s7n")
+        .replace("six", "s6x")
+        .replace("five", "f5e")
+        .replace("four", "f4r")
+        .replace("three", "t3e")
+        .replace("two", "t2o")
+        .replace("one", "o1e")
 }
