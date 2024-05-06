2a9b3d79fc43f17da98696b0736365422e9e9f53
Benjamin <B.Oechsle@web.de>
2024-05-06 22:39:40 +0200

    read data day 7

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,6 +1,13 @@
+import java.io.File
+
 object Day7 {
+    private val cards = mutableMapOf<String, Int>()
+
     @JvmStatic
     fun main(args: Array<String>) {
+        val file = File("src/main/resources/day7/inputTest")
+        file.readLines().map { line -> line.split(" ") }.map { (k,v) -> cards[k] = v.toInt() }
 
+        println(" $cards")
     }
 }
