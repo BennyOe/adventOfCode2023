aa1ad1387b5af8cb1b83ce33d59845daa60cd20e
Benjamin <b.oechsle@web.de>
2024-05-07 17:22:09 +0200

    Day 7 part one not completed

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,13 +1,89 @@
 import java.io.File
 
+data class Hand(val cards: String, val money: Int, val sortOrder: Int) {
+    constructor(cards: String, money: Int) : this(cards, money, calculateSortOrder(cards))
+
+    companion object {
+        fun calculateSortOrder(cards: String): Int {
+            return getDeckValue(cards)
+        }
+
+        private fun getDeckValue(card: String): Int {
+            return when {
+                isFiveOfAKind(card) -> 600
+                isFourOfAKind(card) -> 500
+                isFullHouse(card) -> 400
+                isThreeOfAKind(card) -> 300
+                isTwoPairs(card) -> 200
+                isOnePair(card) -> 100
+                else -> 0
+            }
+        }
+
+        private fun getCardValue(card: Char): Int {
+            return when (card) {
+                'A' -> 20
+                'K' -> 19
+                'Q' -> 18
+                'J' -> 17
+                'T' -> 16
+                else -> card.digitToInt()
+            }
+        }
+
+        private fun isFiveOfAKind(card: String) =
+            countCharacters(card).containsValue(5)
+
+        private fun isFourOfAKind(card: String) =
+            countCharacters(card).containsValue(4)
+
+        private fun isFullHouse(card: String): Boolean {
+            val countMap = countCharacters(card)
+            return countMap.containsValue(3) && countMap.containsValue(2)
+        }
+
+        private fun isThreeOfAKind(card: String): Boolean {
+            val countMap = countCharacters(card)
+            return countMap.containsValue(3) && !countMap.containsValue(2)
+        }
+
+        private fun isTwoPairs(card: String): Boolean {
+            val countMap = countCharacters(card)
+            val countTwo = countMap.count { (_, v) -> v == 2 }
+            return countTwo == 2
+        }
+
+        private fun isOnePair(card: String): Boolean {
+            val countMap = countCharacters(card)
+            val countTwo = countMap.count { (_, v) -> v == 2 }
+            val countOne = countMap.count { (_, v) -> v == 1 }
+            return countTwo == 1 && countOne == 3
+        }
+
+        private fun countCharacters(string: String): Map<Char, Int> {
+            val result = mutableMapOf<Char, Int>()
+            string.forEach { char ->
+                if (!result.containsKey(char)) result[char] = 0
+                if (result.containsKey(char)) result[char] = result[char]!! + 1
+            }
+            return result
+        }
+    }
+}
+
 object Day7 {
-    private val cards = mutableMapOf<String, Int>()
 
     @JvmStatic
     fun main(args: Array<String>) {
         val file = File("src/main/resources/day7/inputTest")
-        file.readLines().map { line -> line.split(" ") }.map { (k,v) -> cards[k] = v.toInt() }
+        val decks = file.readLines().map { line -> line.split(" ") }.map { (k, v) -> Hand(k, v.toInt()) }
+            .sortedBy { it.sortOrder }.toList()
 
-        println(" $cards")
+        var sum = 0
+        for ((index, hand) in decks.withIndex()) {
+            println(hand)
+            sum += (index + 1) * hand.money
+        }
+        println(sum)
     }
 }
