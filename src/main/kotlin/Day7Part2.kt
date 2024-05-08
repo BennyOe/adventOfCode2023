35b649d3b48a927c1434e1e30c63768d77fa7ec7
Benjamin <b.oechsle@web.de>
2024-05-08 12:24:41 +0200

    Day 7 part one completed Part two almost

--- src/main/kotlin/Day7Part2.kt
+++ src/main/kotlin/Day7Part2.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-object Day7 {
+object Day7Part2 {
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -10,6 +10,8 @@ object Day7 {
 
         for (i in decks.indices) {
             for (j in 1 until decks.size) {
+                val di = decks[i].cards
+                val dj = decks[j].cards
                 if (decks[i].sortOrder == decks[j].sortOrder) {
                     if (decks[i].isBetterThan(decks[j].cards)) decks[i].cardOrder += 1
                 }
@@ -17,13 +19,15 @@ object Day7 {
         }
         decks = decks.sortedWith(compareBy(HandPart2::sortOrder, HandPart2::cardOrder))
 
+        decks.forEach { println(it) }
+
         val sum = decks.withIndex().sumOf { (index, hand) -> (index + 1) * hand.money }
         println(sum)
     }
 
 }
 
-data class Hand(val cards: String, val money: Int, var sortOrder: Int, var cardOrder: Int) {
+data class HandPart2(val cards: String, val money: Int, var sortOrder: Int, var cardOrder: Int) {
     constructor(cards: String, money: Int) : this(cards, money, calculateSortOrder(cards), 0)
 
     fun isBetterThan(other: String): Boolean {
@@ -41,8 +45,8 @@ data class Hand(val cards: String, val money: Int, var sortOrder: Int, var cardO
             'A' -> 20
             'K' -> 19
             'Q' -> 18
-            'J' -> 17
             'T' -> 16
+            'J' -> 1
             else -> card.digitToInt()
         }
     }
@@ -50,14 +54,18 @@ data class Hand(val cards: String, val money: Int, var sortOrder: Int, var cardO
     companion object {
         fun calculateSortOrder(cards: String): Int {
             val groupedCards = cards.groupBy { it }
+            val jokerCount = groupedCards['J']?.size?: 0
             val counts = groupedCards.values.map { it.size }
             return when {
-                counts.contains(5) -> 600
-                counts.contains(4) -> 500
+                jokerCount == 5 -> 600
+                counts.contains(5 - jokerCount)  -> 600
+                counts.contains(4 - jokerCount) -> 500
                 counts.contains(3) && counts.contains(2) -> 400
-                counts.contains(3) -> 300
+                counts.count { it == 2} == 2 && jokerCount == 1 -> 400
+                counts.contains(3 - jokerCount) -> 300
                 counts.count { it == 2 } == 2 -> 200
                 counts.count { it == 2 } == 1 && counts.count { it == 1 } == 3 -> 100
+                jokerCount == 1 -> 100
                 else -> 0
             }
         }
